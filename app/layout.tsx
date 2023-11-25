9f233e3b76a4a10b2d77cdb7b2cea57de9456406
Marcelo ChamanMallqui <marcechaman@gmail.com>
2023-11-25 16:59:55 -0500

    lfg

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,23 +24,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/* <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:type" content="website"></meta>
-        <meta property="og:url" content="https://www.marcelochaman.ca" />
-        <meta name="theme-color" content="#000000" />
-        <meta
-          property="og:image"
-          content="https://www.marcelochaman.ca/opengraph-image.png"
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="marcelochaman.ca" />
-        <meta property="twitter:url" content="https://marcelochaman.ca/" />
-        <meta
-          name="twitter:image"
-          content="https://www.marcelochaman.ca/twitter-image.png"
-        ></meta>
-      </head> */}
       <body className="h-[100dvh] w-[100dvw] xl:overflow-hidden flex flex-col lg:flex-row no-scrollbar">
         {children}
         <Navbar />
