0d7c641e1246f75148d8d8ac39a0e71b91795d86
Marcelo ChamanMallqui <marcechaman@gmail.com>
2023-11-24 19:06:44 -0500

    f me

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Navbar from "./components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://www.marcelochaman.com"),
+  metadataBase: new URL("https://www.marcelochaman.ca"),
   title: "Marcelo Chaman Mallqui",
   description: "A digital representation.",
   openGraph: {
