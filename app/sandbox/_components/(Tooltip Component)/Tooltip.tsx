6d579ab6d491b522a14c753bf28e1986299fb492
Marcelo ChamanMallqui <marcechaman@gmail.com>
2024-01-25 15:35:59 -0500

    Add Tooltip component and TooltipContext
    This commit adds a new Tooltip component and the corresponding TooltipContext. The Tooltip component provides a tooltip functionality with customizable content, and the TooltipContext is used to share the tooltip content across components.

---
+++ app/sandbox/_components/(Tooltip Component)/Tooltip.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import TooltipContext from "./TooltipContext";
+
+interface TooltipProps {
+  children: React.ReactNode;
+  content: string;
+}
+
+const Tooltip = ({ children, content }: TooltipProps) => {
+  if (!content) {
+    throw new Error("Tooltip must have content");
+  }
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const contextValue = { content };
+  return (
+    <TooltipContext.Provider value={contextValue}>
+      {isOpen ? (
+        <div className="bg-black text-white py-2 px-4 absolute top-0 translate-y-1/2 z-10 rounded-md">
+          {content}
+        </div>
+      ) : null}
+      <div
+        onMouseEnter={() => setIsOpen(true)}
+        onMouseLeave={() => setIsOpen(false)}
+      >
+        {children}
+      </div>
+    </TooltipContext.Provider>
+  );
+};
+
+export default Tooltip;
---
+++ app/sandbox/_components/(Tooltip Component)/TooltipContext.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface TooltipContextType {
+  content: string;
+}
+
+const defaultContextValue: TooltipContextType = { content: "" };
+
+const TooltipContext =
+  React.createContext<TooltipContextType>(defaultContextValue);
+
+export default TooltipContext;
