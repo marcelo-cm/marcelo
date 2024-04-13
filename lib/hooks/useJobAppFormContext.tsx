a235b559b5e41b1043288377a571a288e9758b8f
Marcelo ChamanMallqui <marcechaman@gmail.com>
2024-04-12 21:17:43 -0400

    oops. had to deleted imports that referred to deleted components.

--- lib/hooks/useJobAppFormContext.tsx
+++
@@ -1,6 +0,0 @@
-import { JobAppFormContext } from "@/app/workman/JobAppForm/JobAppFormContext";
-import { useContext } from "react";
-
-export const useJobAppFormContext = () => {
-  return useContext(JobAppFormContext);
-};
