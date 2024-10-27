0777c277a19886dc731a28f58e4a1d054dbc6769
Marcelo ChamanMallqui <marcechaman@gmail.com>
2024-10-27 19:47:18 +0100

    delete unused code

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Work from '@/components/new/work/Work';
-import Projects from '@/components/new/work/projects/Projects';
+import Work from '@/components/work/Work';
+import Projects from '@/components/work/projects/Projects';
 
 const page = () => {
   return (
